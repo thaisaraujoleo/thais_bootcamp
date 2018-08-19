5f63cafdb45f28c2f2dd71eb98d5ac40f3eb7471
Thais <thaisaraujoleonardo@gmail.com>
2018-08-19 13:13:00 -0300

    Seguidores

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :tweets
+  
+  has_many :active_relationships,  class_name:  "Relationship",
+           foreign_key: "follower_id", dependent:   :destroy
+ 
+  has_many :passive_relationships, class_name:  "Relationship",
+           foreign_key: "followed_id", dependent:   :destroy
+  
+  has_many :following, through: :active_relationships,  source: :followed
+ 
+  has_many :followers, through: :passive_relationships, source: :follower
+  
+  def following?(other_user)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180818191509) do
+ActiveRecord::Schema.define(version: 20180819131317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
