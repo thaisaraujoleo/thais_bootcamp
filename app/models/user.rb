2e0652e5c62bdc18637ceeb4ed33491a14930663
Thais <thaisaraujoleonardo@gmail.com>
2018-08-19 15:04:30 -0300

    User follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,19 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :follower
   
   def following?(other_user)
+    #Este método checa se um usuário está seguindo outro.
+    followers.include? other_user
+  end
+
+  def follow!(other_user)
+    #Este método criará o relacionamento entre um usuário e outro.
+    #active_relationships.create(followed: other_user)
+    following << other_user
+  end
+
+  def unfollow!(other_user)
+    #Este método apagará o relacionamento entre um usuário e outro.
+    following.destroy(other_user
+  end
+
 end
